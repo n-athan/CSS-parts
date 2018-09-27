09f20787feeb873db979a5ea2540c3d85f9e1dca
n-athan <dump.nathan@gmail.com>
2018-09-27 12:43:38 +0200

    added slider and weblayout

--- brickify/canvasversie/script.js
+++ brickify/canvasversie/script.js
@@ -1,130 +1,117 @@
-// function to make random integer in range
-const rnd = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-// create a random color in hsl
+//input from controls
+var pix = document.getElementById("pix");
+var pixOutput = document.getElementById("demo");
+// pixValue.innerHTML = slider.value; 
+
+// Update the current slider value (each time you drag the slider handle)
+pix.oninput = function() {
+    // pixOutput.innerHTML = this.value;
+    pixValue = this.value;
+    var imageObj = loadImage(context,pixValue,source);
+} 
+
+var source = 'fly.jpg';
+// var pixValue = 30;
+var context = initContext();
+
+
+// hsl values to css-hsl
 function base(h,s,l) {
     return(`hsl(${h},${s}%,${l}%)`);
 };
-//Create a canvas to place a pixelated image on. 
-//https://codepen.io/jakealbaugh/post/canvas-image-pixel-manipulation 
 
-function initContext()
-{
+//initialize
+function initContext() {
     var canvas = document.createElement("canvas");
     canvas.width = 800;
     canvas.height = 800;
-    document.body.appendChild(canvas);
-   var context = canvas.getContext('2d');
-   return context;
-}
+    document.body.getElementsByTagName('main')[0].appendChild(canvas);
+    var context = canvas.getContext('2d');
+    return context;
+}; 
 
-// context.width = imageObj.width;
-// context.height = imageObj.height;
-
-
-function loadImage(context)
-{
+//load image
+function loadImage(context,pix,source) {
     var imageObj = new Image();
-    imageObj.onload = function()
-    {
-        var pixelation = 30;
-        var fw = (imageObj.width / pixelation)|0,
-        fh = (imageObj.height / pixelation)|0;
-        context.imageSmoothingEnabled =
-        context.mozImageSmoothingEnabled =
-        context.msImageSmoothingEnabled =
-        context.webkitImageSmoothingEnabled = false;
+    imageObj.onload = function() {
+        var fw = (imageObj.width / pix)|0,
+        fh = (imageObj.height / pix)|0;
+        disableSmoothing(context);
         context.drawImage(imageObj, 0, 0, fw, fh);
         var imageData = context.getImageData(0,0,fw,fh);
+        document.getElementsByTagName('canvas')[0].width = `${fw*50}`;
+        document.getElementsByTagName('canvas')[0].height = `${fh*50}`;
         brickify(imageData,fw);
     };
-    imageObj.src = 'fly.jpg';
+    imageObj.src = source;
     return imageObj;
-}
-var context = initContext();
-var imageObj = loadImage(context);
+};
 
-function brickify(imageData,fw) {
+//to get actual square pixels
+function disableSmoothing() {
+    context.imageSmoothingEnabled =
+            context.mozImageSmoothingEnabled =
+            context.msImageSmoothingEnabled =
+            context.webkitImageSmoothingEnabled = false;
+};
+
+//pixelcolor to HSL
+function dataToHsl(imageData) {   
     var data = imageData.data;
     var colors = [];
-    for(var i = 0, loop = data.length; i < loop; i += 4) {
-      var color = {
-        r: data[i],
-        g: data[i + 1],
-        b: data[i + 2],
-        a: data[i + 3]
-      };
-      colors.push(color);
+    for(var i = 0; i < data.length; i += 4) {
+    var color = rgbToHsl(data[i], data[i + 1], data[i + 2])
+    colors.push(color);
     }
-    
-    //convert pixel colors to hsl
-    var colorsHSL = [];
-    for (i in colors) {
-        var pixel = colors[i];
-        var hsl = rgbToHsl(pixel['r'],pixel['g'],pixel['b'])
-        colorsHSL.push(hsl);
-    };
+    return colors;
+};
 
-    var length = colorsHSL.length;
-    var i = 0;
-    var pixel = 1;
-    var line = 0;
-    
-
-    for (var pixel = 1;pixel <= length;pixel++) {
-        var c = colorsHSL[pixel];
+function brickify(imageData,fw) {
+    var colors = dataToHsl(imageData);
+    var length = colors.length;
+    //loop through all pixels
+    for (var p = 0, i = 0, line = 0; p < length; p++) {
+        var c = colors[p];
         var h = c[0];
         var s = c[1];
         var l = c[2];
-        if (i === fw*25){
+
+        if (i === fw*50){
             i = 0;
-            line = line + 25;
-        };
+            line = line + 50;
+        }
+
         //shadow square
         context.fillStyle = base(h,s,(l-20));
-        context.fillRect(i, line, i+25, line+25);
+        context.fillRect(i, line, i+50, line+50);
         //square
         context.fillStyle = base(h,s,l);
-        context.fillRect(i+1, line+1, i+25, line+25);
+        context.fillRect(i+2, line+2, i+50, line+50);
         //shadow top
         context.beginPath();
         context.fillStyle = base(h,s,(l-30));
-        context.arc(i+14, line+15, 9, 0, 2 * Math.PI, false);
+        context.arc(i+28, line+28, 18, 0, 2 * Math.PI, false);
         context.fill();
         //top  
         context.beginPath();
         context.fillStyle = base(h,s,l+10);
-        context.arc(i+12.5, line+12.5, 9, 0, 2 * Math.PI, false);
+        context.arc(i+25, line+25, 18, 0, 2 * Math.PI, false);
         context.fill();
         //logo
-        context.font = "italic 0.45em Ubuntu";
+        context.font = "italic 0.7em Ubuntu";
         context.textBaseline = 'middle';
         context.textAlign = 'center';
         context.strokeStyle = base(h,s,l);
-        context.strokeText("logo",i+12.5,line+12.5);
+        context.strokeText("LOGO",i+25,line+25);
         //counter
-        i += 25;
+        i += 50;
     };
 };  
-
-
-
-// Sources:
-// Pixelate image by shrinking it and blowing it back up. 
-// https://jsfiddle.net/fedek6/NgwP2/
-// Get color data of each pixel in the pixelated image
-// https://codepen.io/jakealbaugh/post/canvas-image-pixel-manipulation 
-// Convert rgb color to hsl
-// http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
-// https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image
-// https://stackoverflow.com/questions/32279288/getimagedata-returning-all-zeros
-// https://css-tricks.com/hsl-hsla-is-great-for-programmatic-color-control/
 
 /**
  * Converts an RGB color value to HSL. Conversion formula
  * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes r, g, and b are contained in the set [0, 255] and
+ * Assumes r, g, and b are contained in the set [0, 505] and
  * returns h, s, and l in the set [0, 1].
  *
  * @param   Number  r       The red color value
@@ -152,3 +139,14 @@ function rgbToHsl(r, g, b){
 
     return [h, (s*100), (l*100)];
 };
+
+// Sources:
+// Pixelate image by shrinking it and blowing it back up. 
+// https://jsfiddle.net/fedek6/NgwP2/
+// Get color data of each pixel in the pixelated image
+// https://codepen.io/jakealbaugh/post/canvas-image-pixel-manipulation 
+// Convert rgb color to hsl
+// http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+// https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image
+// https://stackoverflow.com/questions/32279288/getimagedata-returning-all-zeros
+// https://css-tricks.com/hsl-hsla-is-great-for-programmatic-color-control/
